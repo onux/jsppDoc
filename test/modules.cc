a2d47908f2c994882e46ba77cb5b45793a1e5ded
Roger Poon <1066594+rogerpoon@users.noreply.github.com>
2017-09-29 23:44:16 -0700

    Refactor: move generator test function to individual file

--- test/modules.cc
+++ test/modules.cc
@@ -1,26 +1,5 @@
 #include "test.h"
 
-using namespace jspp::parser;
-
-std::unique_ptr<pugi::xml_document> generate(const std::string& code) {
-	std::string path = "";
-	std::unique_ptr<Program> program = jspp::parser::parse(
-		code,
-		path,
-		{ jspp::parser::ParserOpt::PARSE_DOC_COMMENT }
-	);
-
-	jspp::docgen::OutputBuilder output;
-	jspp::docgen::DocVisitor docvisitor(&output);
-	program->accept(&docvisitor);
-	std::string generated = output.getOutput();
-
-	auto xml = std::unique_ptr<pugi::xml_document>(new pugi::xml_document);
-	xml->load_string(generated.c_str());
-
-	return xml;
-}
-
 TEST_CASE("Module Name - One module") {
 	auto xml = generate(
 		R"(
--- test/test.h
+++ test/test.h
@@ -1,3 +1,10 @@
+//
+// Copyright (C) 2017 Onux Corporation. All rights reserved.
+//
+
+#ifndef JSPP_DOCGEN_TEST_H
+#define JSPP_DOCGEN_TEST_H
+
 #include <catch.hpp>
 #include <pugixml.hpp>
 #include "CommentData.h"
@@ -6,3 +13,7 @@
 #include "DocVisitor.h"
 #include "OutputBuilder.h"
 #include "Utils.h"
+
+std::unique_ptr<pugi::xml_document> generate(const std::string& code);
+
+#endif
