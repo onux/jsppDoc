fa38ca5ecade727c1764ab098f3f15772afa9431
Roger Poon <1066594+rogerpoon@users.noreply.github.com>
2017-09-29 23:07:30 -0700

    Tests for correct module name generation

--- test/modules.cc
+++ test/modules.cc
@@ -1,11 +1,8 @@
-/*#include "test.h"
+#include "test.h"
 
-TEST_CASE("Module Documentation") {
-	std::string code = R"(
-		module Foo.Bar.Baz
-		{
-		}
-	)";
+using namespace jspp::parser;
+
+std::unique_ptr<pugi::xml_document> generate(const std::string& code) {
 	std::string path = "";
 	std::unique_ptr<Program> program = jspp::parser::parse(
 		code,
@@ -16,8 +13,50 @@ TEST_CASE("Module Documentation") {
 	jspp::docgen::OutputBuilder output;
 	jspp::docgen::DocVisitor docvisitor(&output);
 	program->accept(&docvisitor);
+	std::string generated = output.getOutput();
 
-	SECTION("FQN") {
-		REQUIRE("" == "");
+	auto xml = std::unique_ptr<pugi::xml_document>(new pugi::xml_document);
+	xml->load_string(generated.c_str());
+
+	return xml;
+}
+
+TEST_CASE("Module Documentation - One module") {
+	auto xml = generate(
+		R"(
+			/**
+			 */
+			module Foo
+			{
+			}
+		)"
+	);
+
+	SECTION("Ensure only one module node gets documented") {
+		size_t count = xml->select_nodes("module").size();
+		REQUIRE(count == 1);
 	}
-}*/
+	SECTION("FQN") {
+		REQUIRE(std::string(xml->child("module").child_value("title")) == "Foo");
+	}
+}
+
+TEST_CASE("Module Documentation - Multiple modules separated by dot (.)") {
+	auto xml = generate(
+		R"(
+			/**
+			 */
+			module Foo.Bar.Baz
+			{
+			}
+		)"
+	);
+
+	SECTION("Ensure only one module node gets documented") {
+		size_t count = xml->select_nodes("module").size();
+		REQUIRE(count == 1);
+	}
+	SECTION("FQN") {
+		REQUIRE(std::string(xml->child("module").child_value("title")) == "Baz");
+	}
+}
--- test/test.h
+++ test/test.h
@@ -3,4 +3,6 @@
 #include "CommentData.h"
 #include "CommentParser.h"
 #include "DocCommentTags.h"
+#include "DocVisitor.h"
+#include "OutputBuilder.h"
 #include "Utils.h"
